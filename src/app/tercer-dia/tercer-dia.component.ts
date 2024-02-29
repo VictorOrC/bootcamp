ac61ac1da5d8b68526147c7b67f485bf954cf934
VictorOrC <vict.rduno@gmail.com>
2024-02-29 12:24:30 -0700

    Agregamos una tabla y utilizamos el service para llenarla

--- src/app/tercer-dia/tercer-dia.component.ts
+++ src/app/tercer-dia/tercer-dia.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ExampleService } from '../service/example.service';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tercer-dia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule, ReactiveFormsModule,],
   templateUrl: './tercer-dia.component.html',
   styleUrl: './tercer-dia.component.scss'
 })
 export class TercerDiaComponent {
+  name = ''
+  users:any = []
+  constructor(public exampleService: ExampleService){
 
+  }
+
+  ngOnInit() {
+    this.exampleService.obtenerLista().subscribe(resp => {
+      console.log(resp);
+      this.users = resp;
+    })
+  }
+
+  editartexto(text:string){
+    console.log(text)
+    return text;
+  }
 }
